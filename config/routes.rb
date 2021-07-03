4c9fb2e0ed6379e70c814e970bc2b618c6344ed9
Troy Russell <troyjruss@gmail.com>
2021-07-03 22:01:33 +0900

    tasks working ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # List all tasks
+  get 'tasks', to: 'tasks#index'
+  # create a task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # list a single task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
+  # edit a task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
 end
